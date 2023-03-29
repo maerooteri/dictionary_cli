4d04079781a6199f06c63102fcd1a88a8e652e28
maerooteri <maerooteri@yahoo.com>
2023-03-29 11:58:53 +0100

    Improving the code

--- dictionary.py
+++ dictionary.py
@@ -4,36 +4,47 @@ import sys
 
 load_dotenv()
 
+
+def get_word_definition(word):
+    try:
+        url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}"
+        response = requests.get(url)
+        response_status = response.status_code
+        # print(response_status)
+        if response_status == 200:
+            data = json.loads(response.text)
+            # print(data)
+
+            if data and type(data[0]) is dict:
+                try:
+                    pronunciation = data[0].get('hwi').get('prs')[0].get('mw')
+                    pronunciation = pronunciation.strip("ˈ").strip("ˌ").strip(",")
+                except Exception:
+                    pronunciation = ''
+                try:
+                    part_of_speech = data[0].get('fl')
+                except Exception:
+                    part_of_speech = ''
+                meaning = data[0].get('shortdef')[0]
+                meaning = meaning.split(':')[0].strip()
+
+                final_output = f"{pronunciation} ({part_of_speech}): {meaning}"
+            elif type(data) is list:
+                final_output = f"This word definition doesn't exist in this dictionary" 
+            else:
+                final_output = f"This word definition doesn't exist in this dictionary"
+
+        return final_output
+    except Exception:
+       return "An Exception occured!!" 
+
 api_key = os.environ.get('API_KEY')
-print(api_key)
-word = input('Enter a word you want to search for: ')
 
-url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}"
 
-response = requests.get(url)
-response_status = response.status_code
-
-data = json.loads(response.text)
-
-if data and type(data[0]) is dict:
-    try:
-        pronunciation = data[0].get('hwi').get('prs')[0].get('mw')
-        pronunciation = pronunciation.strip("ˈ").strip("ˌ").strip(",")
-    except Exception:
-        pronunciation = ''
-    try:
-        part_of_speech = data[0].get('fl')
-    except Exception:
-        part_of_speech = ''
-    meaning = data[0].get('shortdef')[0]
-    meaning = meaning.split(':')[0].strip()
-
-    final_output = f"{pronunciation} ({part_of_speech}): {meaning}"
-elif type(data) is list:
-    """This gives Homonyms (similar words in the dictionary),
-      if the word we searched for doesnt exist"""
-    final_output = f"This word definition doesn't exist in\
-          this dictionary, did you mean any of these ? \n {data}"
-else:
-    final_output = f"Word definition doesn't exist in this dictionary"
-print(final_output)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        word = sys.argv[1]
+    else:
+        word = input('Enter a word you want to search for: ')
+    output = get_word_definition(word)
+    print(output)
